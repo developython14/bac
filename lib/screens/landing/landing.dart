d504ed3773a794daf0d410750dcf707d2a5c78ab
developython14 <djmel1955@gmail.com>
2023-03-28 19:05:18 +0200

    launch intiliastion

--- lib/screens/landing/landing.dart
+++ lib/screens/landing/landing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mucap/screens/landing/componants/story.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:mucap/screens/landing/home.dart';
 
 class Landing extends StatefulWidget {
   const Landing({Key? key}) : super(key: key);
@@ -40,54 +41,5 @@ class _LandingState extends State<Landing> {
         appBar: AppBar(title: Text('gtr')),
       ),
     );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(
-      children: [
-        Text(
-          'Stories',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              storybutton(),
-              storybutton(),
-              storybutton(),
-              storybutton(),
-              storybutton(),
-              storybutton(),
-              storybutton()
-            ],
-          ),
-        ),
-        Text(
-          'المستويات ',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-        ),
-        Container(
-          color: Colors.green,
-          child: const Material(
-            child: ListTile(
-              title: Text(
-                'السنة الثالثة ثانوي ',
-                style: TextStyle(fontSize: 26),
-              ),
-              trailing: Text('fd'),
-            ),
-          ),
-        )
-      ],
-    ));
   }
 }
