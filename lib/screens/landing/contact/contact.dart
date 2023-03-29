ba7f89b1ac0adc7e80045e5c07495ccc6cb9a7b0
developython14 <djmel1955@gmail.com>
2023-03-29 17:05:36 +0200

    launch intiliastion

--- lib/screens/landing/contact/contact.dart
+++ lib/screens/landing/contact/contact.dart
@@ -1,1 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:rightservices/screen/providers/translations_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
+
+final Uri _url_email =
+    Uri.parse('mailto: S7QURAN-S@hotmail.com?subject=help&body=New%question');
+final Uri website = Uri.parse('https://rightservices.herokuapp.com/');
+final Uri _url_phone = Uri.parse('tel:+966-563027472');
+final Uri _url_sms = Uri.parse('sms:+966-563027472');
+
+Future<void> _launchUrl_email() async {
+  if (!await launchUrl(_url_email)) {
+    throw 'Could not launch $_url_email';
+  }
+}
+
+Future<void> _launchUrl_phone() async {
+  if (!await launchUrl(_url_phone)) {
+    throw 'Could not launch $_url_phone';
+  }
+}
+
+Future<void> _launchUrl_web() async {
+  if (!await launchUrl(website)) {
+    throw 'Could not launch $website';
+  }
+}
+
+Future<void> _launchUrl_sms() async {
+  if (!await launchUrl(_url_sms)) {
+    throw 'Could not launch $_url_sms';
+  }
+}
+
+class contatc extends StatelessWidget {
+  const contatc({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: context.read<Translator>().gettext('direction'),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff3A4750),
+          title: Text(
+            context.read<Translator>().gettext('conatctus').toString(),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(context.read<Translator>().gettext('web')),
+              Card(
+                  child: ListTile(
+                onTap: _launchUrl_web,
+                leading: Icon(Icons.web),
+                title: Text('RightServices.com'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              )),
+              Text(
+                context.read<Translator>().gettext('email'),
+              ),
+              Card(
+                  child: ListTile(
+                onTap: _launchUrl_email,
+                leading: Icon(Icons.email),
+                title: Text('S7QURAN-S@hotmail.com'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              )),
+              Text(
+                context.read<Translator>().gettext('phone'),
+              ),
+              Card(
+                  child: ListTile(
+                onTap: _launchUrl_phone,
+                leading: Icon(Icons.phone),
+                title: Text('+966-56-302-7472'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              )),
+              Text(
+                context.read<Translator>().gettext('sms'),
+              ),
+              Card(
+                  child: ListTile(
+                onTap: _launchUrl_sms,
+                leading: Icon(Icons.sms),
+                title: Text('+966-56-302-7472'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
