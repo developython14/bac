8aca7e0f419c513a690a17618e284bf0d4ff1237
developython14 <djmel1955@gmail.com>
2023-03-29 17:51:13 +0200

    add contact apge and theirs permission

--- lib/screens/landing/payment/payment.dart
+++ lib/screens/landing/payment/payment.dart
@@ -8,8 +8,27 @@ class payment extends StatelessWidget {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        body: Column(
-          children: [Text('يمكنك الدفع باستخدام Baridi Mob')],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('يمكنك الدفع باستخدام Baridi Mob'),
+              Card(
+                  child: ListTile(
+                onTap: () {},
+                leading: Icon(Icons.facebook_outlined),
+                title: Text('00799999001486730070'),
+              )),
+              Text('أو عبر البريد ccp'),
+              Card(
+                  child: ListTile(
+                onTap: () {},
+                leading: Icon(Icons.facebook_outlined),
+                title: Text('001486730070'),
+              )),
+            ],
+          ),
         ),
       ),
     );
