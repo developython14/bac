b8e51a59973eff00fa8dbd566aa0970f9add2ca5
developython14 <djmel1955@gmail.com>
2023-03-29 18:53:13 +0200

    add comments pages

--- lib/screens/landing/comment/comment.dart
+++ lib/screens/landing/comment/comment.dart
@@ -7,9 +7,23 @@ class comments extends StatelessWidget {
   Widget build(BuildContext context) {
     var toma = [2, 2, 2, 2, 2];
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          return Text('free');
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://media.licdn.com/dms/image/C4E03AQG5Jvo_fUP0Sg/profile-displayphoto-shrink_800_800/0/1629473101419?e=1685577600&v=beta&t=ZxDA-KF5Xy5F2LHLQQEIp9SiKaarbN_KDXdaw9uZCFE')),
+              title: Text('Mustapha Belkassem'),
+              subtitle: Text(
+                  'انه تطبيق رائع وانا من بعد كي شفت ال تسجيل الأعضاء يمكنك أن تضيف هذه اللعبة للألعاب بمختلف أنواعها من بعد ما عمر ب ببركان ينددون بسلوك و اخلاق ولللي اي شيئ راسلني اخي انا ثان شكيت واحد من بعد كي شفت'),
+              isThreeLine: true,
+            ),
+          );
         },
         itemCount: toma.length,
       ),
