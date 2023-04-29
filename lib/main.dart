ced749cfb94e2a6d5f359061c13d27ef672d362d
developython14 <djmel1955@gmail.com>
2023-04-29 18:37:47 +0200

    work on adding provider confugrations fro state managements

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:google_fonts/google_fonts.dart';
+// ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -162,36 +166,38 @@ class _MyAppState extends State<MyApp> {
         ? Center(
             child: Text('sorry not run on virtuel machine'),
           )
-        : MaterialApp(
-            title: 'HackElbac',
-            theme: ThemeData(
-                // This is the theme of your application.
-                //
-                // Try running your application with "flutter run". You'll see the
-                // application has a blue toolbar. Then, without quitting the app, try
-                // changing the primarySwatch below to Colors.green and then invoke
-                // "hot reload" (press "r" in the console where you ran "flutter run",
-                // or simply save your changes to "hot reload" in a Flutter IDE).
-                // Notice that the counter didn't reset back to zero; the application
-                // is not restarted.
-                primarySwatch: Colors.blue,
-                textTheme: GoogleFonts.cairoTextTheme(
-                  Theme.of(context).textTheme,
-                )),
-            initialRoute: '/test',
-            routes: {
-              '/': (context) => Landing(),
-              '/stories': (context) => MoreStories(),
-              '/login': (context) => MyLogin(),
-              '/signup': (context) => MyRegister(),
-              '/filieres': (context) => filliers(),
-              '/modules': (context) => modules(),
-              '/profs': (context) => profs(),
-              '/cours': (context) => Cours(),
-              '/faq': (context) => faq(),
-              '/change_photo': (context) => change_photo(),
-              '/test': (context) => ListTileExample(),
-            },
+        : MultiProvider(
+            providers: [],
+            child: MaterialApp(
+              title: 'HackElbac',
+              theme: ThemeData(
+                  // This is the theme of your application.
+                  //
+                  // Try running your application with "flutter run". You'll see the
+                  // application has a blue toolbar. Then, without quitting the app, try
+                  // changing the primarySwatch below to Colors.green and then invoke
+                  // "hot reload" (press "r" in the console where you ran "flutter run",
+                  // or simply save your changes to "hot reload" in a Flutter IDE).
+                  // Notice that the counter didn't reset back to zero; the application
+                  // is not restarted.
+                  primarySwatch: Colors.blue,
+                  textTheme: GoogleFonts.cairoTextTheme(
+                    Theme.of(context).textTheme,
+                  )),
+              initialRoute: '/test',
+              routes: {
+                '/': (context) => Landing(),
+                '/stories': (context) => MoreStories(),
+                '/login': (context) => MyLogin(),
+                '/signup': (context) => MyRegister(),
+                '/filieres': (context) => filliers(),
+                '/modules': (context) => modules(),
+                '/profs': (context) => profs(),
+                '/cours': (context) => Cours(),
+                '/faq': (context) => faq(),
+                '/change_photo': (context) => change_photo(),
+              },
+            ),
           );
   }
 }
@@ -201,4 +207,3 @@ class _MyAppState extends State<MyApp> {
 // found in the LICENSE file.
 
 // ignore_for_file: public_member_api_docs
-
---
+++ lib/providers/stories/stories.dart
@@ -0,0 +1,46 @@
+import 'dart:ffi';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class Storiesproviderd with ChangeNotifier, DiagnosticableTreeMixin {
+  List _list_stories = [];
+  List __services_signup_mog = [];
+
+  List get list_stories => _list_stories;
+  void changecategories(id, name) {
+    notifyListeners();
+  }
+
+  Future<void> getallarticles() async {
+    var ref = await getdata(__services_signup_mog);
+    notifyListeners();
+  }
+
+  /// Makes `Counter` readable inside the devtools by listing all of its properties
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+  }
+}
+
+getdata(ref) async {
+  var test = Uri.parse('base_url' + '/admin/list_services');
+  var response = await http.get(test);
+  List articles = [];
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    print(jsonResponse);
+    for (var i = 0; i < jsonResponse.length; i++) {
+      articles.add(jsonResponse[i]);
+      ref.add(Service(jsonResponse[i]['_id'], jsonResponse[i]['ar'],
+          jsonResponse[i]['en']));
+    }
+    return articles;
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+    return [];
+  }
+}
